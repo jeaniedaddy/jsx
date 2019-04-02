ed07f14295b508a829679d1a84077549afd21c7e
steve <jeaniedaddy@gmail.com>
2019-04-02 13:23:19 -0400

    excercise jsx

--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const name = "Steve No";
-const App = ()=> <h2>{name} Hellow world!</h2>;
+//const App = ()=> <h2>{name} Hellow world!</h2>;
+const App = () => {
+    const aa = 'click me';        
+    return (
+    
+    <div>
+        <label className="label" htmlFor="name">Enter name: </label>
+        <input id="name" />
+        <button style={{backgroundColor: 'blue', color: 'white'}}>{aa}</button>
+    </div>
+    );
+};
+
 
 // Take the react compnent and show it on the screen.  
 ReactDOM.render(
